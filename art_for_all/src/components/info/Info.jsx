69197499e3180b41fc616fd5a6a120a7401b15f1
thaynnara007 <thaynnara.goncalves@ccc.ufcg.edu.br>
2018-11-14 20:33:17 -0300

    stilyzing Info component

--- art_for_all/src/components/info/Info.jsx
+++ art_for_all/src/components/info/Info.jsx
@@ -4,21 +4,33 @@ import './Info.css';
 import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 class Info extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            value: 'adventure'
-        }
-    }
 
     render(){
+
+        const ART_NAME = 0;
+        const AUTHOR = 1;
+        const COLLECTION = 2;
+        const DESCRIPTION = 3;
+
         return(
             <div className="container note">
-                <details>
-                    <summary className="font-weight-bold">Informations</summary>
-                    <p>Art name: Adventure ends</p>
-                    <p>Author: thay@ra</p>
-                    <p>Collection: Adventure time</p>
+                <details className="font">
+                    <summary className="font-weight-bold summary-margin">Informations</summary>
+                    <div className="back">
+                        <label className="label-style">Art Name:</label>
+                    </div>
+                    <p>Adventure ends</p>
+                    <div className="back ">
+                        <label className="label-style">Author:</label>
+                    </div>
+                    <p>thay@ra</p>
+                    <div className="back">
+                        <label className="label-style">Collection:</label>
+                    </div>
+                    <p>Adventure time</p>
+                    <div className="back">
+                        <label className="label-style">Description:</label>
+                    </div>
                     <p>Description: its a art to express the end of the cartoon</p>
                 </details>
                 <TagBox></TagBox>
