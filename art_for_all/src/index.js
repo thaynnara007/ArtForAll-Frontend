c7af718d14e317f422027a5b4efd571251f01a95
thaynnara007 <thaynnara.goncalves@ccc.ufcg.edu.br>
2018-11-15 19:05:00 -0300

    initializing navbar component

--- art_for_all/src/index.js
+++ art_for_all/src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import DetailsPage from './components/detailsPage/DetailsPage';
+import Navbar from './components/navbar/Navbar';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<DetailsPage />, document.getElementById('root'));
+ReactDOM.render(<Navbar />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
