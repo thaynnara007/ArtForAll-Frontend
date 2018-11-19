60c746af744c52f5a4ee22ea31e49c950aea08e7
thaynnara007 <thaynnara.goncalves@ccc.ufcg.edu.br>
2018-11-19 07:18:15 -0300

    initializing EditableDetailsArtPage

---
+++ art_for_all/src/components/detailsPage/EditableDetailsArtPage.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import InfoArt from '../info/InfoArt';
+import image from '../../adventure.jpg';
+import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './EditableDetailsArtPage.css';
+import './DetailsArtPage.css';
+
+class EditableDetailsArtPage extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+
+            information:["Adventure ends", "th@y", "Adventure Time", " Its a art to express the end of the cartoon", "thaynnara.goncalves@ccc.ufcg.edu.br"],
+            tagsName: ['Adventure time', "Finn", "Blood Sword", "Joshua"]
+        }
+    }
+
+    render(){
+        return(
+            <div className="container max style">
+                    <button className="close">x</button>
+                    <div className="flex">
+                        <img className="img-size" src={image} alt="adventure ends"></img>
+                        <section className="info">
+                            <InfoArt info={this.state.information} tagsName={this.state.tagsName}></InfoArt>
+                        </section>
+                    </div>
+                </div>
+        )
+    }
+}
+
+export default EditableDetailsArtPage;
--- art_for_all/src/index.js
+++ art_for_all/src/index.js
@@ -2,15 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import DetailsArtPage from './components/detailsPage/DetailsArtPage';
-import Navbar from './components/navbar/Navbar';
-import TagBox from './components/tagBox/TagBox';
+import EditableDetailsArtPage from './components/detailsPage/EditableDetailsArtPage';
 import * as serviceWorker from './serviceWorker';
-import ETagBox from './components/tagBox/EditableTagBox';
-import InputFild from './components/forms/input/InputFild';
-import EditableInfoArt from './components/info/EditableInfoArt';
 
-ReactDOM.render(<EditableInfoArt/>, document.getElementById('root'));
+
+ReactDOM.render(<EditableDetailsArtPage/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
