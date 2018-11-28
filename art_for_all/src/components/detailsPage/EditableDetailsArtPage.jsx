651844acad56946fd3a4479465b1864407718b36
thaynnara007 <thaynnara.goncalves@ccc.ufcg.edu.br>
2018-11-27 23:13:51 -0300

    stylizing and adding a new behavior ate editableDEtailsPage

--- art_for_all/src/components/detailsPage/EditableDetailsArtPage.jsx
+++ art_for_all/src/components/detailsPage/EditableDetailsArtPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import InfoArt from '../info/InfoArt';
 import EditableInfoArt from '../info/EditableInfoArt';
-import image from '../../adventure.jpg';
+import image from '../../TG5CxZ.jpg';
 import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './EditableDetailsArtPage.css';
 import './DetailsArtPage.css';
@@ -12,9 +12,12 @@ class EditableDetailsArtPage extends React.Component{
         this.state = {
 
             information:["Adventure ends", "th@y", "Adventure Time", " Its a art to express the end of the cartoon", "thaynnara.goncalves@ccc.ufcg.edu.br"],
-            tagsName: ['Adventure time', "Finn", "Blood Sword", "Joshua"]
+            tagsName: ['Adventure time', "Finn", "Blood Sword", "Joshua"],
+            file: image
         }
         this.addTag = this.addTag.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.renderImage = this.renderImage.bind(this);
     }
 
     addTag(tagName){
@@ -28,15 +31,35 @@ class EditableDetailsArtPage extends React.Component{
         }
     }
 
+    handleChange(event){
+        
+        this.setState({
+            information: this.state.information,
+            tagsName: this.state.tagsName,
+            file: URL.createObjectURL(event.target.files[0])
+        })
+    }
+
+    renderImage(){
+        
+        if (this.state.file)
+            return (<img className="img-size" src={this.state.file} alt=""></img>)
+        else return null
+    }
+
     render(){
         return(
             <div className="container max style">
                     <button className="close">x</button>
                     <div className="flex">
-                        <img className="img-size" src={image} alt="adventure ends"></img>
+                        <div>
+                            <div>{this.renderImage()}</div> 
+                            <input className="browser-button" type="file" onChange={this.handleChange}></input> 
+                        </div>
                         <section className="info">
                             <EditableInfoArt info={this.state.information} tagsName={this.state.tagsName} addTag={this.addTag}></EditableInfoArt>
-                        </section>
+                            <button className="btn btn-outline-primary btn-lg btn-block saveButton">Save</button>
+                        </section> 
                     </div>
                 </div>
         )
