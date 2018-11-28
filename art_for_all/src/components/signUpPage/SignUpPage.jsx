86df07bc92120e7dafdd5b580352f3515f64fa54
thaynnara007 <thaynnara.goncalves@ccc.ufcg.edu.br>
2018-11-28 03:42:54 -0300

    sign up page

--- art_for_all/src/components/signUpPage/SignUpPage.jsx
+++ art_for_all/src/components/signUpPage/SignUpPage.jsx
@@ -1,17 +1,36 @@
 import React from 'react'
 import './SignUpPage.css'
-import SingUpForm from '../forms/signUp/signUpForm'
+import Input from '../forms/input/InputFild'
 import Navbar from '../navbar/Navbar'
 import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 class SignUpPage extends React.Component{
-
     render(){
-        return(
+        return (
+
             <div>
                 <Navbar></Navbar>
-                <SingUpForm></SingUpForm>
+                <div className="font container singUpForm-size singUpForm-color">
+                    <div className="singUpForm-header">
+                        <p className="singUpForm-font">Register</p>
+                    </div>
+                    <div className="singUpForm-space">
+                        
+                        <label className="singUpForm-label">First name:</label>
+                        <Input></Input>
+                        <label className="singUpForm-label">Last name:</label>
+                        <Input></Input>
+                        <label className="singUpForm-label">Username:</label>
+                        <Input></Input>
+                        <label className="singUpForm-label">Email:</label>
+                        <Input></Input>
+                        <label className="singUpForm-label">Password:</label>
+                        <Input></Input>
+                        <button type="button" class="btn btn-outline-success btn-lg btn-block spaace">Sign Up</button>
+                        <br></br>
+                    </div>  
             </div>
+        </div>
         )
     }
 
