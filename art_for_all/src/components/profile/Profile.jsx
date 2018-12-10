a98aa20dce6cadf448b821bfc2f17b1508ede8be
thaynnara007 <thaynnara.goncalves@ccc.ufcg.edu.br>
2018-12-10 00:04:57 -0300

    removing behaviors from profilePicture component and profileInfo component, and puting it on Profile component

--- art_for_all/src/components/profile/Profile.jsx
+++ art_for_all/src/components/profile/Profile.jsx
@@ -3,11 +3,33 @@ import Navbar from '../navbar/Navbar';
 import ProfileInfo from '../profileInfo/ProfileInfo';
 import Artbox from '../artBox/ArtBox';
 import './Profile.css';
+import image from '../../img/me3.png';
 
 class Profile extends React.Component{
     constructor(props){
         super(props);
 
+        this.state={
+            headFile: null,
+            profileFile: image,
+            userName: 'Th@y',
+            numberArts: 21,
+            following: 51,
+            followers: 40,
+        }
+        this.editFile = this.editFile.bind(this);
+
+    }
+
+    editFile(event, data){
+        
+        var file = undefined;
+        if(event.target.files[0]) file = URL.createObjectURL(event.target.files[0])
+        else file = this.state.file;
+
+        this.setState({
+            [data]: file
+        })
     }
 
     render(){
@@ -18,7 +40,10 @@ class Profile extends React.Component{
                     <Navbar></Navbar>
                 </div>
                 <div>
-                    <ProfileInfo></ProfileInfo>
+                    <ProfileInfo editFile={this.editFile} 
+                    headFile={this.state.headFile} 
+                    profileFile={this.state.profileFile}
+                    info={[this.state.userName, this.state.numberArts, this.state.following, this.state.followers]}></ProfileInfo>
                 </div>
                 <div className="profile-arts">
                     <Artbox></Artbox>
--- art_for_all/src/components/profileInfo/ProfileInfo.jsx
+++ art_for_all/src/components/profileInfo/ProfileInfo.jsx
@@ -4,35 +4,16 @@ import './ProfileInfo.css';
 import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 class ProfileInfo extends React.Component{
-    constructor(props){
-        super(props);
-
-        this.state={
-            headFile: null,
-            profileFile: null,
-            userName: 'Th@y',
-            numberArts: 21,
-            following: 51,
-            followers: 40,
-        }
-        this.editHeadFile = this.editHeadFile.bind(this);
-    }
-
-    editHeadFile(event){
-        
-        var file = undefined;
-        if(event.target.files[0]) file = URL.createObjectURL(event.target.files[0])
-        else file = this.state.file;
-
-        this.setState({
-            headFile: file
-        })
-    }
 
     render(){
 
+        const USERNAME = 0;
+        const NUMARTS = 1;
+        const FOLLOWING = 2;
+        const FOLLOWERS = 3;
+
         const style ={
-            backgroundImage: 'url(' + (this.state.headFile) + ')',
+            backgroundImage: 'url(' + (this.props.headFile) + ')',
             backgroundPosition: 'center',
             backgroundRepeat: 'no-repeat',
             backgroundSize: 'cover',
@@ -42,21 +23,21 @@ class ProfileInfo extends React.Component{
             backgroundColor: 'lightsteelblue'
         }
 
-        var details = (this.state.numberArts)+ " arts | "+(this.state.following)+" fllowing | "+(this.state.followers)+" followers"
+        var details = (this.props.info[NUMARTS])+ " arts | "+(this.props.info[FOLLOWING])+" fllowing | "+(this.props.info[FOLLOWERS])+" followers";
 
         return(
             <div className="font">
-                <div className='profileInfo-head-img' style={this.state.headFile ? style : style2}>
+                <div className='profileInfo-head-img' style={this.props.headFile ? style : style2}>
                     <div className="float-rigth editableDetailsArtPage-upload">
                         <button className="btn profileInfo-upload">Edit</button>
-                        <input type="file" name="myfile" onChange={this.editHeadFile}/>
+                        <input type="file" name="myfile" onChange={(event, data) => this.props.editFile(event, 'headFile')}/>
                     </div>
                     <button className="btn float-rigth profileInfo-aboutButton">About</button>
                     <button className="btn float-rigth profileInfo-followButton">Follow</button>
                     <div className="float-left profileInfo-profileFile">
-                        <ProfilePicture>{this.state.profileFile}</ProfilePicture>
+                        <ProfilePicture editFile={this.props.editFile}>{this.props.profileFile}</ProfilePicture>
                     </div>
-                    <p className="profileInfo-username float-left">{this.state.userName}</p>
+                    <p className="profileInfo-username float-left">{this.props.info[USERNAME]}</p>
                 </div>
                 <p className="profileInfo-details">{details}</p>
             </div>
