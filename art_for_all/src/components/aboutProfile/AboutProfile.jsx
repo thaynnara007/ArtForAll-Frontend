d2e1e5f35f14659e0823dca82d97d7ddc3a198f0
thaynnara007 <thaynnara.goncalves@ccc.ufcg.edu.br>
2018-12-11 00:07:05 -0300

    adding some behavior at aboutProfile component

--- art_for_all/src/components/aboutProfile/AboutProfile.jsx
+++ art_for_all/src/components/aboutProfile/AboutProfile.jsx
@@ -1,34 +1,69 @@
 import React from "react";
 import ProfilePicture from '../profilePicture/ProfilePicture';
+import Navbar from '../navbar/Navbar';
 import Input from '../forms/input/InputFild';
 import Text from '../forms/textArea/Text';
+import image from '../../img/me3.png';
 import './AboutProfile.css';
 
-
 class AboutProfile extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.state={
+            userName:"Th@y",
+            profileFile: image,
+            firstName: "Thaynnara",
+            lastName: "Gonçalves",
+            email: "thaynnara.goncalves@ccc.ufcg.edu.br",
+            contact: 'whatsApp, telegram: 8393333-3333',
+            description: 'I am a brazilian artist in my free time :)'
+        }
+        this.editProfilePicture = this.editProfilePicture.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    editProfilePicture(event){
+        
+        var file = undefined;
+        if(event.target.files[0]) file = URL.createObjectURL(event.target.files[0]);
+        else file = this.state.profileFile;
+
+        this.setState({
+            profileFile: file
+        })
+    }
+
+    handleChange(event, data){
+        this.setState({
+            [data]:event.target.value
+        })
+    }
+
     render(){
         return(
             <div className="font">
+                <Navbar logado={true} userName={this.state.userName}></Navbar>
                 <div className="aboutProfile-container">
                     <div className="aboutProfile-head">
                         <p className="aboutProfile-p">About</p>
                     </div>
                     <div className="aboutProfile-picture">
-                        <ProfilePicture editFile={this.props.editFile}>{this.props.image}</ProfilePicture>
+                        <ProfilePicture editFile={this.editProfilePicture}>{this.state.profileFile}</ProfilePicture>
                     </div>
                     <div className="singUpForm-space">
                         <label className="singUpForm-label">First name:</label>
-                        <Input handleChange={this.props.handleChange} data="firstName"></Input>
+                        <Input handleChange={this.handleChange} data="firstName" placeHolder={this.state.firstName}></Input>
                         <label className="singUpForm-label">Last name:</label>
-                        <Input handleChange={this.props.handleChange} data="lastName"></Input>
+                        <Input handleChange={this.handleChange} data="lastName" placeHolder={this.state.lastName}></Input>
                         <label className="singUpForm-label">Username:</label>
-                        <Input handleChange={this.props.handleChange} data="userName"></Input>
+                        <Input handleChange={this.handleChange} data="userName" placeHolder={this.state.userName}></Input>
                         <label className="singUpForm-label">Email:</label>
-                        <Input handleChange={this.props.handleChange} data="email"></Input>
+                        <Input handleChange={this.phandleChange} data="email" placeHolder={this.state.email}></Input>
                         <label className="singUpForm-label">Contact:</label>
-                        <Text handleChange={this.props.handleChange} data="contact"></Text>
+                        <Text handleChange={this.handleChange} data="contact" placeHolder={this.state.contact}></Text>
                         <label className="singUpForm-label">Description:</label>
-                        <Text handleChange={this.props.handleChange} data="description"></Text>
+                        <Text handleChange={this.handleChange} data="description" placeHolder={this.state.description}></Text>
                         <button type="button" className="btn btn-outline-success btn-lg btn-block spaace">Save changes</button>
                     </div>
                     <br></br>
--- art_for_all/src/index.js
+++ art_for_all/src/index.js
@@ -17,7 +17,7 @@ import AboutPage from './components/abouPage/About';
 import AboutProfile from './components/aboutProfile/AboutProfile';
 
 
-ReactDOM.render(<AboutProfile/>, document.getElementById('root'));
+ReactDOM.render(<AboutProfile open={true}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
