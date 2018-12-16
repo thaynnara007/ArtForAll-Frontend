bea5eb1edc5b81c376841b5341a40eb148fbec12
thaynnara007 <thaynnara.goncalves@ccc.ufcg.edu.br>
2018-12-16 17:30:31 -0300

    adding inputFild.test

--- art_for_all/src/test/InputFild.test.js
+++ art_for_all/src/test/InputFild.test.js
@@ -1,8 +1,15 @@
+import React from 'react';
 import Jest from 'jest';
 import Enzyme from './Enzyme';
+import { shallow} from 'enzyme';
 import InputFild from '../components/forms/input/InputFild';
 
-it('render correctly text component', () => {  
-    const TextInputComponent = renderer.create(<InputFild />).toJSON();
-    expect(TextInputComponent).toMatchSnapshot();
+it('render correctly input fild component with type=text', () => {  
+    const InputFildComponent = shallow(<InputFild />);
+    expect(InputFildComponent).toMatchSnapshot();
+});
+
+it('render correctly input fild component with type=password', () => {  
+    const InputFildComponent = shallow(<InputFild type="password"/>);
+    expect(InputFildComponent).toMatchSnapshot();
 });
